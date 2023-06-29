000101d4752dc75f29eb53b49b5ee1fb5e7f6de9
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-06-29 18:40:41 -0500

    updated logo/favicon, changed styling for copyButton, fixed error in IntersectionObserver that showed up as unhandled promise rejection

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "@/styles/layout/Header.module.css";
 
@@ -6,12 +7,14 @@ export default function Header(): JSX.Element {
   return (
     <div className={styles.container}>
       <div className={styles.imageWrapper}>
-        <Image
-          src="/giphy-logo.svg"
-          alt="GIPHY Logo"
-          className={styles.logo}
-          fill
-        />
+        <Link href="/">
+          <Image
+            src="/giphy-logo.png"
+            alt="GIPHY Logo"
+            className={styles.logo}
+            fill
+          />
+        </Link>
       </div>
     </div>
   );
